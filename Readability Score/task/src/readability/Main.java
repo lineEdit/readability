b00f39eca3e3e90ab7b4ccdc60a31e97c7f095b3
lineedit <lineedit@gmail.com>
2020-04-07 14:08:44 +0500

    Stage 2, end.

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.nextLine().length() > 100 ? "HARD" : "EASY");
+        String[] string = scanner.nextLine().split("[!.?]");
+        int mid = 0;
+        for (String item : string) {
+            mid += item.split("\\s+").length;
+        }
+        System.out.println(mid/string.length > 10 ? "HARD" : "EASY");
     }
 }
